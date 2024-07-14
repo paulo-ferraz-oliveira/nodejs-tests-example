2bf9ae0868b10a5a5f609035f198e00ec24d3148
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-07-15 00:06:37 +0100

    Act on test results
    We wanna be able to instantiate the object on a browser

--- src/nodejs-tests-example.js
+++ src/nodejs-tests-example.js
@@ -1,41 +1,43 @@
-/* global PubSub, module */
+// The contents of the globalThis should be directly exported for browser usage
+
 globalThis.PubSub = (() => {
 	'use strict';
-
-	this.listeners = {};
-	this.on = (eventName, listener) => {
-		if (!this.listeners[eventName]) {
-			this.listeners[eventName] = [];
-		}
-		this.listeners[eventName].push(listener);
-	};
-	this.once = (eventName, listener) => {
-		const onceWrapper = (...args) => {
-			listener.apply(this, args);
-			this.off(eventName, onceWrapper);
+	return () => {
+		this.listeners = {};
+		this.on = (eventName, listener) => {
+			if (!this.listeners[eventName]) {
+				this.listeners[eventName] = [];
+			}
+			this.listeners[eventName].push(listener);
 		};
-		this.on(eventName, onceWrapper);
-	};
-	this.off = (eventName, listener) => {
-		if (!this.listeners[eventName]) {
-			return;
-		}
-		this.listeners[eventName] = this.listeners[eventName].filter(
-			(needle) => needle !== listener,
-		);
-	};
-	this.emit = (eventName, ...args) => {
-		if (!this.listeners[eventName]) {
-			return;
-		}
-		this.listeners[eventName].forEach((listener) => {
-			listener.apply(this, args);
-		});
-	};
+		this.once = (eventName, listener) => {
+			const onceWrapper = (...args) => {
+				listener.apply(this, args);
+				this.off(eventName, onceWrapper);
+			};
+			this.on(eventName, onceWrapper);
+		};
+		this.off = (eventName, listener) => {
+			if (!this.listeners[eventName]) {
+				return;
+			}
+			this.listeners[eventName] = this.listeners[eventName].filter(
+				(needle) => needle !== listener,
+			);
+		};
+		this.emit = (eventName, ...args) => {
+			if (!this.listeners[eventName]) {
+				return;
+			}
+			this.listeners[eventName].forEach((listener) => {
+				listener.apply(this, args);
+			});
+		};
 
-	return this;
+		return this;
+	};
 })();
 
 module.exports = {
-	PubSub,
+	PubSub: globalThis.PubSub,
 };
