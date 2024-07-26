3c05dc8b8057e88fd66cc102058e996c9aa286b5
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2024-07-26 18:16:16 +0100

    Make it prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,14 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 
 export default [
-	{
-		files: ['**/*.js'],
-		languageOptions: { sourceType: 'script' },
-	},
-	{
-		languageOptions: {
-			globals: globals.node,
-		},
-	},
-	pluginJs.configs.all,
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'script' },
+  },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  pluginJs.configs.all,
 ];
